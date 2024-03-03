24658bed3cb523567ce2ff013105202a9961e84e
ViktoR <Kachamakov@abv.bg>
2024-03-03 22:15:07 +0200

    Chef page added

---
+++ Culinary/Controllers/ChefController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Culinary.Controllers
+{
+    public class ChefController : Controller
+    {
+        public IActionResult Chef()
+        {
+            return View();
+        }
+    }
+}
