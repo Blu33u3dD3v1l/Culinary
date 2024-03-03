674f72633c2026f59c41d6702e1daa4f964310ec
ViktoR <Kachamakov@abv.bg>
2024-03-03 21:55:29 +0200

    Events page added

---
+++ Culinary/Controllers/EventController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Culinary.Controllers
+{
+    public class EventController : Controller
+    {
+        public IActionResult Event()
+        {
+            return View();
+        }
+    }
+}
